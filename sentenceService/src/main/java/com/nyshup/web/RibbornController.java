9e92a6a5df45c9772fb54c1f082ad25e08be6625
Ruslan Pushyn <nyshup@gmail.com>
2017-02-14 15:37:35 +0200

    Imerge to one wordservice, user rebborn

--- sentenceService/src/main/java/com/nyshup/web/RibbornController.java
+++ sentenceService/src/main/java/com/nyshup/web/RibbornController.java
@@ -2,35 +2,34 @@ package com.nyshup.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
-public class SentenceController {
+public class RibbornController {
 
     @Autowired
-    DiscoveryClient client;
+    LoadBalancerClient client;
 
-    @RequestMapping("/sentence2")
+    @RequestMapping("/sentence")
     public @ResponseBody
     String getSentence() {
         return getWord("subject-service") + " "
-                        + getWord("verb-service") + " "
-                        + getWord("article-service") + " "
-                        + getWord("adjective-service") + " "
-                        + getWord("noun-service") + ".";
+                + getWord("verb-service") + " "
+                + getWord("article-service") + " "
+                + getWord("adjective-service") + " "
+                + getWord("noun-service") + ".";
     }
 
     public String getWord(String service) {
-        List<ServiceInstance> list = client.getInstances(service);
-        if (list != null && list.size() > 0 ) {
-            URI uri = list.get(0).getUri();
+        ServiceInstance instance = client.choose(service);
+        if (instance != null) {
+            URI uri = instance.getUri();
             if (uri !=null ) {
                 return (new RestTemplate()).getForObject(uri,String.class);
             }
